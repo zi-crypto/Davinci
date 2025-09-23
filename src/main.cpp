6b4300da7069679cd403cf6373fb9b00461c211d
Ziad Mohammed Amer <60266435+zi-crypto@users.noreply.github.com>
2025-09-23 16:22:00 +0300

    Implemented The Grayscale Filter, Encountered Multiple Definition Linker Errors, Solved By Editing Image_Class.h and Creating An Implementation Only File

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,25 @@
 // Entry Point
 // Here we will Implement the Menu + Integration
+#include <iostream>
+#include "Image_Class.h"
+#include "filters.h"
+
+using namespace std;
+
+int main() {
+    string filename;
+    cout << "Pls enter colored image name to turn to gray scale: ";
+    cin >> filename;
+
+    Image image(filename);
+
+    // Call your filter
+    applyGrayscaleFilter(image);
+
+    cout << "Pls enter image name to store new image (with extension): ";
+    cin >> filename;
+
+    image.saveImage(filename);
+
+    return 0;
+}
