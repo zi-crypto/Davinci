21b1e724f289498a8d58635d4e3c2ef7842779b5
Hexogen <nazhim.alakkash@gmail.com>
2025-09-24 16:32:52 +0300

    commented out the function calls

--- src/main.cpp
+++ src/main.cpp
@@ -17,23 +17,23 @@ int main() {
     //..rotation etc.. to apply that, the function will be called recursively from main.cpp
 
     // ================= Samir's Filters =================
-    applyGrayscale(image);  // Filter 1
-    applyMerge(image, image2);  // Filter 4
-    applyDarkenImage(image, 0.5f);  // Filter 7a
-    applyLightenImage(image, 1.5f);  // Filter 7b
-    applyAddColoredFrame(image, 5, 255, 0, 0);  // Filter 10
-
-    // ================= Ziad's Filters =================
-    applyBlackWhite(image);  // Filter 2
-    applyFlipImage(image, true);  // Filter 5 (horizontal flip)
-    applyCropImage(image, 10, 10, 100, 100);  // Filter 8
-    applyDetectEdges(image);  // Filter 11
-
-    // ================= Ammen's Filters =================
-    applyInvertColors(image);  // Filter 3
-    applyRotateImage(image, 90);  // Filter 6
-    applyResizeImage(image, 200, 300);  // Filter 9
-    applyApplyBlur(image, 5);  // Filter 12
+    // applyGrayscale(image);  // Filter 1
+    // applyMerge(image, image2);  // Filter 4
+    // applyDarkenImage(image, 0.5f);  // Filter 7a
+    // applyLightenImage(image, 1.5f);  // Filter 7b
+    // applyAddColoredFrame(image, 5, 255, 0, 0);  // Filter 10
+    //
+    // // ================= Ziad's Filters =================
+    // applyBlackWhite(image);  // Filter 2
+    // applyFlipImage(image, true);  // Filter 5 (horizontal flip)
+    // applyCropImage(image, 10, 10, 100, 100);  // Filter 8
+    // applyDetectEdges(image);  // Filter 11
+    //
+    // // ================= Ammen's Filters =================
+    // applyInvertColors(image);  // Filter 3
+    // applyRotateImage(image, 90);  // Filter 6
+    // applyResizeImage(image, 200, 300);  // Filter 9
+    // applyApplyBlur(image, 5);  // Filter 12
  
     cout << "Pls enter image name to store new image (with extension): ";
     cin >> filename;
