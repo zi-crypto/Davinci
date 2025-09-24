1f8eb6ecb66d7ae585bdeed301e47154e962ba1f
Hexogen <nazhim.alakkash@gmail.com>
2025-09-24 16:26:22 +0300

    edit to filter/h

--- include/filters.h
+++ include/filters.h
@@ -6,25 +6,25 @@
 #include "Image_Class.h"
 
 
+// ================= Samir's Filters =================
+void applyGrayscale(Image &image);  // Filter 1
+void applyMerge(Image &image1, const Image &image2);  // Filter 4
+// Filter 7 (two parts)
+void applyDarkenImage(Image &image, float factor);
+void applyLightenImage(Image &image, float factor);
+void applyAddColoredFrame(Image &image, int thickness, int r, int g, int b);  // Filter 10
 
-void applyGrayscale_Filter_1(Image &image);
-void applyBlackWhite_Filter_2(Image &image);
-void applyInvertColors_Filter_3(Image &image);
+// ================= Ziad's Filters =================
+void applyBlackWhite(Image &image);  // Filter 2
+void applyFlipImage(Image &image, bool horizontal);  // Filter 5
+void applyCropImage(Image &image, int x, int y, int width, int height);  // Filter 8
+void applyDetectEdges(Image &image);  // Filter 11
 
-void applyMerge_Filter_4(Image &image1, const Image &image2);
-void applyFlipImage_Filter_5(Image &image, bool horizontal);
-void applyRotateImage_Filter_6(Image &image, int angle);
-
-void applyDarkenImage_Filter_7(Image &image, float factor);
-void applyLightenImage_Filter_7(Image &image, float factor);
-void applyCropImage_Filter_8(Image &image, int x, int y, int width, int height);
-
-void applyAddColoredFrame_Filter_9(Image &image, int thickness, int r, int g, int b);
-void applyDetectEdges_Filter_10(Image &image);
-void applyResizeImage_Filter_11(Image &image, int newWidth, int newHeight);
-
-void applyApplyBlur_Filter_12(Image &image, int intensity);
-
+// ================= Ammen's Filters =================
+void applyInvertColors(Image &image);  // Filter 3
+void applyRotateImage(Image &image, int angle);  // Filter 6
+void applyResizeImage(Image &image, int newWidth, int newHeight);  // Filter 9
+void applyApplyBlur(Image &image, int intensity);  // Filter 12
 
 
 #endif // FILTERS_H
